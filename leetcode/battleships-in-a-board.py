efdd8672ab746eb4ac61912db01b5b14ede48875
Yuriy Ivanov <yivanov@arbfile.org>
2019-09-19 16:52:03 -0400

    xyu

--- leetcode/battleships-in-a-board.py
+++ leetcode/battleships-in-a-board.py
@@ -12,21 +12,18 @@ class Solution(object):
                 if board [i][j] == 'X':
                     print ('i=%i, j=%i, board[i][j]=%s' % (i, j, board [i][j]))
                     xcount = 0
-                    # print (max(0,i-1), min(wboard-1,i+1)-1, max(0,i-1), min(wboard,i+1)-1)
-                    for x in (max(0,i-1), min(wboard-1,i+1)):
-                        for y in (max(0,j-1), min(hboard-1,j+1)):
-                            if x != i and y != j:
-                                print ('x=%i, y=%i, board[x][j]=%s' % (x, y, board [x][y]))
-                                if board [x][j] == 'X' or board [i][y]:
-                                    xcount =+ 1
+                    for c in list (filter (lambda x: x[0] in range (wboard) and x[1] in range (hboard), [[i+x,j+y] for x in [1,-1] for y in [1, -1]])):
+                        print ('c=', c, board [c[0]][c[1]])
+                        if board [c[0]][c[1]] == 'X':
+                            xcount += 1
                     if xcount == 0:
                         print ('Evrika!!! i=%i j=%i xcount=%i' % (i, j, xcount))
                         result += 1
-                    else:
+                    elif xcount == 1:
                         print ('Xyevrika!!! i=%i j=%i xcount=%i' % (i, j, xcount))
                         result += 0.5
                     
-        return result
+        return int(result)
                 # print (board [i][j])
 
 z = Solution()
@@ -36,4 +33,17 @@ b = [
         ['.','.','.','X'],
         ['X','X','.','.'],
     ]
+# b = [
+#     ["X",".",".","X"],
+#     [".",".",".","X"],
+#     [".",".",".","X"]
+# ]
+# b = [["X","X","X"]]
 print (z.countBattleships(b))
+
+# i, j, wboard, hboard = 1, 3, 3, 4
+# for x in list (filter (lambda z: z >= 0 and z < wboard, [i-1, i+1])):
+#     print (x)
+#     for y in list (filter (lambda z: z >= 0 and z < hboard, [j-1, j+1])):
+#         print (y, b [x][j], b [i][y])
+        
