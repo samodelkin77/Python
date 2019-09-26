3e7001c25b74de9a8ef7477b8561b43e9588e3eb
Yuriy Ivanov <yivanov@arbfile.org>
2019-09-25 22:43:50 -0400

    xyu

--- leetcode/integer-to-english-words.py
+++ leetcode/integer-to-english-words.py
@@ -1,7 +1,7 @@
 from math import log
 
 class Solution(object):
-    def selfText (self):
+    def selfTest (self):
         tests = [
             {"input":123, "output":"One Hundred Twenty Three"},
             {"input":12345, "output":"Twelve Thousand Three Hundred Forty Five"},
@@ -14,19 +14,42 @@ class Solution(object):
         for test in tests:
             result = self.numberToWords (test["input"])
             if result != test["output"]:
-                print ("Zhopa. Input:", test["input"], ' output:', result, ' expected:', test["output"])
+                print ("Zhopa. Input:", test["input"], ' output:"', result, '" expected:"', test["output"], '"')
 
     def numberToWords(self, num):
         """
         :type num: int
         :rtype: str
         """
+        if num == 0:
+            return 'Zero'
         result = ''
-        numerals = ['', 'One', 'Two', 'Three', 'Four', 'Six', 'Seven', 'Eight', 'Nine']
-        second
-        twodigits = ['Ten', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']]
-        triples = ['Hundred', 'Thousand', 'Million', 'Billion']
-        for i in range (1, log (num, 10) / 3 + 1):
-            
-            result = ;
+        numerals = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+        teens = ['Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen']
+        tens = ['', '', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
+        triples = ['', 'Thousand', 'Million', 'Billion']
+        for i in range (int(log (num, 10) / 3 + 1) + 1):
+            n = (int)((num % pow (10, (i + 1) * 3) / pow (10, i * 3)))
+            print ('i=%i n=%i' % (i, n))
+            t = ''
+            if n > 0:
+                # if n % 100 != 0:
+                if n % 100 in range (10, 20):
+                    t = teens [n % 100 - 10]
+                else:
+                    if n % 10 > 0:
+                        t = numerals [n % 10]
+                    if n % 100 in range (20, 100):
+                        if len (t) > 0:
+                            t = ' ' + t 
+                        t = tens [(int)((n % 100) / 10)] + t
+                if n >= 100:
+                    if len (t) > 0:
+                        t = ' ' + t 
+                    t = numerals [(int)(n / 100)] + ' Hundred' + t
+                result = t + ' ' + triples [i] + ' ' + result.strip()
+        return result.strip()
         
+z = Solution()
+# z.selfTest()
+print (z.numberToWords (100000))
--- leetcode/minimum-window-substring.py
+++ leetcode/minimum-window-substring.py
@@ -7,23 +7,43 @@ class Solution(object):
         """
         result = s + '#'
         currPos = 0
+        insPos = 0
         # firstPos = 0
         foundChars = []
         lookingForChars = list (t)
         while currPos < len (s):
-            print (currPos, s[currPos], foundChars)
+            if insPos < len (foundChars) and currPos > foundChars [insPos]["pos"]:
+                insPos += 1
+            print (currPos, s[currPos], foundChars, insPos)
             if s[currPos] in lookingForChars:
-                foundChars.append ({"pos":currPos, "char":s[currPos]})
+                # foundChars.append ({"pos":currPos, "char":s[currPos]})
+                foundChars.insert (insPos, {"pos":currPos, "char":s[currPos]})
                 lookingForChars.remove (s[currPos])
                 if len (lookingForChars) == 0:
+                    print ('Evrika!!! ', s[foundChars[0]["pos"]:foundChars[-1]["pos"]+1])
                     if len (result) > foundChars[-1]["pos"] - foundChars[0]["pos"]:
                         result = s[foundChars[0]["pos"]:foundChars[-1]["pos"]+1]
                     currPos = foundChars[0]["pos"]
                     lookingForChars.extend (foundChars[0]["char"])
-                    foundChars.remove (foundChars[0])
+                    foundChars.remove (foundChars[0])   
+                    insPos = 0
             currPos += 1
-        return result
+        if len(result) > len (s):
+            return ''
+        else:
+            return result
 
 z = Solution()
 
-print (z.minWindow('ADOBECODEBANC', 'ABC'))
+# print (z.minWindow('ADOBECODEBANC', 'ABC'))
+print (z.minWindow('aa', 'aa'))
+
+#0123456789012
+#ADOBECODEBANC
+
+A: 0,10
+B: 3,9
+C: 5,12
+
+# 0123456789012
+# A  B C   BA C
