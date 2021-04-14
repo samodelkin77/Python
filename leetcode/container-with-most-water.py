152311e8d8ca56491178c85570401dac88ba943a
Yuriy Ivanov <yivanov@arbfile.org>
2021-04-14 17:03:32 -0400

    asdasda

--- leetcode/container-with-most-water.py
+++ leetcode/container-with-most-water.py
@@ -1,15 +1,32 @@
 class Solution:
-    
+
+    def findMinMax (self, h):
+
+        def setValue (a, b, func):
+            if func (h [a], h [b]):
+                return a
+            else:
+                return b
+
+        mm = [{"min":-1, "max":-1}, {"min":0, "max":0}]
+        for i in range (2, len (h)):
+            j = i-1
+            mm.append ({"min": setValue (j, mm [j] ["min"], lambda x, y: x < y), "max": setValue (j, mm [j] ["max"], lambda x, y: x >= y)})
+
+        return mm
+        
+
     def maxArea(self, height) -> int:
-        m = 0
-        for i, e in enumerate (height[:-1]):
-            if (len (height) - i) * e > m:
-                for j in range (i+1, len (height)):
-                    if min (e, height[j]) * (j - i) > m:
-                        m = min (e, height[j]) * (j - i)
-        return m
+        mm = self.findMinMax (height)
+        for i in range (len (height)):
+            print (i, height [i], mm [i] ["min"], mm [i] ["max"])
+        return -1
+
 
 
 z = Solution()
 
-print (z.maxArea([1,8,6,2,5,4,8,3,7]))
+#print (z.maxArea([1,8,6,2,5,4,8,3,7]))
+#print (z.maxArea([1, 2, 4, 3]))
+print (z.maxArea([8,10,14,0,13,10,9,9,11,11]))
+#print (z.maxArea([1, 2]))
